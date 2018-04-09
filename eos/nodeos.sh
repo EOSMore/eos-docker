f15984091c90635adf068d72aaffa57f336a5bd0
shrimp <liu.shrimp@gmail.com>
2018-04-09 17:55:00 +0800

    智能合约路径及日志输出

--- eos/nodeos.sh
+++ eos/nodeos.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 cd /opt/eosio/bin
 
-if [ -d '/opt/eosio/bin/data-dir/contracts' ]; then
-    echo
-  else
-    cp -r /contracts /opt/eosio/bin/data-dir
-fi
+DATA_DIR=/opt/eosio/bin/data-dir
+CONFIG_DIR=/opt/eosio/bin/config-dir
 
-exec /opt/eosio/bin/nodeos --data-dir=/opt/eosio/bin/data-dir --config-dir=/opt/eosio/bin/config-dir $@
+exec /opt/eosio/bin/nodeos --data-dir $DATA_DIR --config-dir $CONFIG_DIR > $DATA_DIR/stderr.txt 2> $DATA_DIR/stdout.txt $@
